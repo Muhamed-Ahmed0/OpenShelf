e52a798d0bce84826c81ce4035fbad4ddbefacd0
Muhamed <muhamedahmed1243p@gmail.com>
2025-01-30 14:28:53 +0200

    Fixed Signout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,27 @@
-import { cn } from "@/lib/utils";
-import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { Button } from "./ui/button";
-import { signOut } from "next-auth/react";
+import Image from "next/image";
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
   return (
     <header className="my-10 flex justify-between gap-5">
       <Link href="/">
-        <Image src={"/icons/logo.svg"} alt="logo" width={100} height={100} />
+        <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
       </Link>
-      <ul className="flex items-center gap-8">
+
+      <ul className="flex flex-row items-center gap-8">
         <li>
           <form
             action={async () => {
               "use server";
+
               await signOut();
             }}
             className="mb-10"
           >
             <Button>Logout</Button>
           </form>
-          {/* <Link href="/my-profile">
-            <Avatar>
-              <AvatarFallback className="bg-amber-100 font-extrabold">
-                {getInitials(session?.user?.name || "UK")}
-              </AvatarFallback>
-            </Avatar>
-          </Link> */}
         </li>
       </ul>
     </header>
